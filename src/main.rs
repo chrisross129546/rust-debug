518592aefcde2894b3f54c844c2a8aaea4806ec5
chris@propriotec.com <chris@propriotec.com>
2024-07-14 22:13:23 +0100

    added some iterable debugging code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    let mut test: i8 = 5;
-    println!("test = {}", test);
 
+}
+
+fn get_primitive_iterable_example() {
+    let vector_example: Vec<&str> = vec!["nexus", "bagels", "is", "gay"];
+    println!("{:?}", vector_example);
+
+    let array_example: [&str; 4] = ["nexus", "bagels", "is", "gay"];
+    println!("{:?}", array_example);
+
+    let tuple_example: (&str, &str, &str, &str) = ("nexus", "bagels", "is", "gay");
+    println!("{:?}", tuple_example);
 }
