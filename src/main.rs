f143e9e3daec3b366c3c8f20d58e12f03dd4a95c
chris@propriotec.com <chris@propriotec.com>
2024-07-14 23:08:44 +0100

    iterable example updated

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-
+    get_primitive_iterable_example()
 }
 
 fn get_primitive_iterable_example() {
     let vector_example: Vec<&str> = vec!["nexus", "bagels", "is", "gay"];
-    println!("{:?}", vector_example);
+    println!("{:?} {}", vector_example, vector_example.capacity());
 
     let array_example: [&str; 4] = ["nexus", "bagels", "is", "gay"];
     println!("{:?}", array_example);
